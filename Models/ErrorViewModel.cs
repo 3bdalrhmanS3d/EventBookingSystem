913bc10647a0ee68faa343e3ad362bcdbada3fd2
3bdalrhmanS3d <abdalrhmansaad24@gmail.com>
2025-05-07 18:38:52 +0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EventBookingSystemV1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
