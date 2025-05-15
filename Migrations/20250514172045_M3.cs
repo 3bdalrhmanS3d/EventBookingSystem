bda1fb2a52794f85d63ea83689f8ef5b1e80cb6a
3bdalrhmanS3d <abdalrhmansaad24@gmail.com>
2025-05-15 22:06:08 +0300

    Add migration for organizer contact info and new image
    Added a migration to include `OrganizerEmail` and `OrganizerPhone`
    columns in the `Events` table. Updated the designer file and
    `ApplicationDbContextModelSnapshot` to reflect these schema changes.
    Also added a new binary file, `CampSoon.png`.

---
+++ Migrations/20250514172045_M3.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EventBookingSystemV1.Migrations
+{
+    /// <inheritdoc />
+    public partial class M3 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "OrganizerEmail",
+                table: "Events",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "OrganizerPhone",
+                table: "Events",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "OrganizerEmail",
+                table: "Events");
+
+            migrationBuilder.DropColumn(
+                name: "OrganizerPhone",
+                table: "Events");
+        }
+    }
+}
